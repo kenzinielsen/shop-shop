f48533991d54aa51d7e0f26d5634f442f75c9d0d
Kenzi Nielsen <kenzinielsen@gmail.com>
2021-12-17 14:07:46 -0700

    indexdb

--- client/src/components/ProductItem/index.js
+++ client/src/components/ProductItem/index.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { pluralize } from "../../utils/helpers"
 import { useStoreContext } from '../../utils/GlobalState';
 import { ADD_TO_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
+import { idbPromise } from "../../utils/helpers";
 
 function ProductItem(item) {
   const [state, dispatch] = useStoreContext();
@@ -16,11 +17,16 @@ function ProductItem(item) {
         _id: _id,
         purchaseQuanitity: parseInt(itemInCart.purchaseQuanitity) + 1
       })
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuanitity: parseInt(itemInCart.purchaseQuanitity) + 1
+      })
     } else {
     dispatch({
       type: ADD_TO_CART,
       product: { ...item, purchaseQuanitity: 1 }
     })
+    idbPromise('cart', 'put', {...item, purchaseQuanitity: 1});
   }
   }
   const {
